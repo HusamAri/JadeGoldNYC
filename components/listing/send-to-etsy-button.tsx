bb2e61f6a4e29b4726be0e02b330e43ee825902b
Husam Ari <husamari@gmail.com>
2026-09-06 09:50:37 +0300

    fix: anchor Etsy gallery action

--- components/listing/send-to-etsy-button.tsx
+++ components/listing/send-to-etsy-button.tsx
@@ -91,6 +91,7 @@ export function SendToEtsyButton({
 
   return (
     <Button
+      id="etsy-gallery-action"
       type="button"
       variant={armed ? "destructive" : "outline"}
       onClick={send}
